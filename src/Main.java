f1493c448241bd3349a9be6ae38e8d3c9f48a187
Yulia Kobeleva <kobelevaulia@gmail.com>
2021-03-23 21:33:33 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        long cost = 34500;
+        int amount = 20;
+
+        long bonus = cost / amount;
+        System.out.println(bonus);
+    }
+}
